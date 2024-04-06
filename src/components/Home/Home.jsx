c78486e59124afcb354ae4c278f05a2dcdd22b56
arfanur04 <arfanur04@gmail.com>
2024-04-06 17:09:15 +0600

    51-4

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,7 @@
-import { useContext } from "react";
-import { AuthContext } from "../../providers/AuthProvider";
-
 const Home = () => {
-	const authInfo = useContext(AuthContext);
-	console.log(`authInfo:`, authInfo);
-
 	return (
 		<div>
-			<h2>This is Home: {authInfo.name}</h2>
+			<h2>This is Home: </h2>
 		</div>
 	);
 };
